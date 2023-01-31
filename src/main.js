640f177f113e6862bd5639886f27d64d179e446c
rana-wagdi <ranawagdi4@gmail.com>
2023-01-31 16:49:42 +0200

    add nav

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 
-library.add(faCircle, faTwitter, faWhatsapp)
+library.add(faCircle, faTwitter, faWhatsapp, faMagnifyingGlass)
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).mount('#app')
